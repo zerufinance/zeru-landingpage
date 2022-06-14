f578a623947100e59c4a6083b8b7ef88075274af
Aviroxi <gurugkgurukiran@gmail.com>
2022-06-14 15:19:36 +0530

    changed coming soon button

--- src/components/LandingPage/TopNavbar.js
+++ src/components/LandingPage/TopNavbar.js
@@ -5,10 +5,10 @@ import logo from '../../images/logo.png'
 import name from '../../images/name.png'
 
 const menuitem = {
-  background:'transparent',
+  background: 'transparent',
 }
 const anchoritem = {
-  color:'white'
+  color: 'white'
 }
 
 export default class TopNavbar extends Component {
@@ -18,39 +18,40 @@ export default class TopNavbar extends Component {
     return (
       <div>
         <img src={logo} width={30} style={{ float: 'left', marginTop: '15px' }} />
-        <img src={name} width={130} style={{ float: 'left', marginTop: '11px',marginLeft:'7px' }} />
-        
-        <Menu mode="horizontal" style={{ float: 'right', backgroundColor: 'transparent', color: 'white',borderBottom:'none'}}>
-            <Menu.Item key="mail" style={menuitem}>
+        <img src={name} width={130} style={{ float: 'left', marginTop: '11px', marginLeft: '7px' }} />
+
+        <Menu mode="horizontal" style={{ float: 'right', backgroundColor: 'transparent', color: 'white', borderBottom: 'none' }}>
+          <Menu.Item key="mail" style={menuitem}>
             <a href='#fifthview' style={anchoritem}>
               Zero Collateral Loans
             </a>
-            
-            </Menu.Item>
+          </Menu.Item>
           <Menu.Item key="1" style={menuitem}>
             <a href='#secondview' style={anchoritem}>
               DFI + NFT
-          </a>
-              
-            </Menu.Item>
+            </a>
+
+          </Menu.Item>
           <Menu.Item key="2" style={menuitem}>
             <a href='#thirdview' style={anchoritem}>
               Transferable NFT
-          </a>
-              
-            </Menu.Item>
+            </a>
+
+          </Menu.Item>
           <Menu.Item key="3" style={menuitem}>
             <a href='#forthview' style={anchoritem}>
               Community
-          </a>
+            </a>
           </Menu.Item>
           <Menu.Item key="4" style={menuitem}>
-            <Button id='seeproductbtn' type='primary' style={{ float: 'right', marginTop: '10px', borderRadius: '0.75rem',border:'none',height:'40px' }}>
+            <a id='seeproductbtn' style={{ marginTop: '10px', borderRadius: '0.75rem', border: 'none', height: '50px',color:'white',padding:'7px'}}>
               Coming soon
-            </Button>
+            </a>
           </Menu.Item>
+
         </Menu>
-       
+
+
       </div>
     )
   }
