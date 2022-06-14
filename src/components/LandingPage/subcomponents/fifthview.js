f88e88c56e54a3713f78ac0f790cc431d86db16c
Aviroxi <gurugkgurukiran@gmail.com>
2022-06-14 10:24:36 +0530

    changed second page

--- src/components/LandingPage/subcomponents/fifthview.js
+++ src/components/LandingPage/subcomponents/fifthview.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import { Row, Col } from 'antd';
-import credit from '../../../images/credit.png'
-import freeupcollateral from '../../../images/freeupcollateral.png'
+import credit from '../../../images/credit1.png'
+import freeupcollateral from '../../../images/freeupcollateral1.png'
 import users from '../../../images/user.png'
 import institution from '../../../images/institution.png'
+import community from '../../../images/community1.png'
 import { Fade } from 'react-awesome-reveal';
 import Feature from './minicomps/Feature';
 import '../../../css/second.css'
@@ -13,7 +14,7 @@ export default class fifthview extends Component {
     render() {
         return (
             <div id='fifthview'>
-                <Fade direction='up' duration={500} triggerOnce='true' fraction={1}>
+                {/* <Fade direction='up' duration={500} triggerOnce='true' fraction={1}>
                     <div className='heading'>
                         Zero Collateral Loans
                         <div className='gradientlinebottom'>
@@ -40,71 +41,77 @@ export default class fifthview extends Component {
                    
                     </Fade>
                     
-                </div>
-                
-                {/* <Fade direction='up' duration={500} triggerOnce='true' fraction={1}>
+                </div> */}
+
+                <Fade direction='up' duration={500} triggerOnce='true' fraction={1}>
                     <div className='heading'>
                         Zero Collateral Loans
-                        <p style={{ fontSize: '1rem' }}>Enabling Zero collateral loans to everyone in the world</p>
+                        <div className='gradientlinebottom'>
+                        </div>
+                        <p style={{ fontSize: '1.1rem', fontWeight: '500' }}>Enabling Zero collateral loans to everyone in the world</p>
                     </div>
-                    
+
                 </Fade>
 
                 <div className='secondcenter'>
                     <Fade direction='left' duration={500} triggerOnce='true' fraction={1}>
                         <div className='d1'>
-                            Get line of credit
+                            <div className='gradientline'>
+                            </div>
+                            Get Line of Credit
                             <p>
-                                orem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content.
+                                Get Credit instantly from zeru, We will never take any personal information from the user and we will never ask for KYC.
                             </p>
                         </div>
                     </Fade>
                     <Fade direction='right' duration={500} triggerOnce='true' fraction={1}>
                         <div className='d2'>
-                            <img src={Credit} alt="get credit" width={150} />
+                            <img src={credit} alt="get credit" width={150} />
                         </div>
                     </Fade>
-                    
+
                 </div>
 
                 <div className='secondcenter'>
                     <Fade direction='left' duration={500} triggerOnce='true' fraction={1}>
                         <div className='d2'>
-                            <img src={collateral} alt="get credit" width={150} />
+                            <img src={freeupcollateral} alt="get credit" width={150} />
                         </div>
                     </Fade>
                     <Fade direction='right' duration={500} triggerOnce='true' fraction={1}>
                         <div className='d1'>
-                            Free up your collateral
+                            <div className='gradientline'>
+                            </div>
+                            Free up Your Collateral
                             <p>
-                                orem Ipsum, sometimes referred to as 'lipsum', is the placeholder text used in design when creating content.
+                                Your collateral will get freed everytime you take a loan, and eventually when you reach zero collateral we don't need collateral to give you loan.
                             </p>
                         </div>
                     </Fade>
-                    
+
                 </div>
 
                 <div className='secondcenter'>
                     <Fade direction='left' duration={500} triggerOnce='true' fraction={1}>
                         <div className='d1'>
-                            Retail + Institutional
+                            <div className='gradientline'>
+                            </div>
+                            Community First
                             <p>
-                                Loans are given out to both Retail Borrowers and Institutional Borrowers alike.
+                                Zeru will be community governed protocol, Decisions will be made by community.
                             </p>
                         </div>
                     </Fade>
                     <Fade direction='right' duration={500} triggerOnce='true' >
                         <div className='d2'>
                             <div className='id1'>
-                                <img src={users} alt="get credit" width={150} />
-                                +
-                                <img src={institution} alt="get credit" width={150} />
+                                <img src={community} alt="get credit" width={150} />
                             </div>
                         </div>
                     </Fade>
-                   
-                </div> */}
-                
+
+                </div>
+
             </div>
         )
     }
