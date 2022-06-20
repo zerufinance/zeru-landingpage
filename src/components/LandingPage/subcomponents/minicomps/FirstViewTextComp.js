757b58393cefe3234c1917e198ec00bac3b60fd7
Aviroxi <gurugkgurukiran@gmail.com>
2022-06-20 17:20:55 +0530

    a

--- src/components/LandingPage/subcomponents/minicomps/FirstViewTextComp.js
+++ src/components/LandingPage/subcomponents/minicomps/FirstViewTextComp.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import '../../../../css/styles.css'
+import '../../../../css/span.css'
 
 export default class FirstViewTextComp extends Component {
   render() {
     return (
       
       <div id='firstviewtextcomp'>
-        <span className=''>Credit Industry
+        <span className='spanfirstview'>Credit Industry
          of Defi World with 
-         <span className='gradientext'>Zero-Collateral Loans</span> </span>
+         <span className='gradientext spanfirstview'>Zero-Collateral Loans</span> </span>
         <p>
         Bringing Credit Industry into Decentralized Finance World <br/>Enabling Zero Collateral Loans for everyone.
         </p>
