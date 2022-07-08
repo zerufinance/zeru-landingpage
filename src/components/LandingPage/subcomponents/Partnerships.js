94f76bdf3fd23f1f146c3d0c085cc0eb41619b39
Aviroxi <gurugkgurukiran@gmail.com>
2022-07-08 18:13:40 +0530

    added partner with us get in touch button

--- src/components/LandingPage/subcomponents/Partnerships.js
+++ src/components/LandingPage/subcomponents/Partnerships.js
@@ -2,12 +2,15 @@ import { Button } from 'antd'
 import React from 'react'
 import eth from '../../../images/eth.png'
 import bitcoin from '../../../images/bitcoin.png'
+import DiscordButton from './reusable/DiscordButton'
 
 export default function Partnerships() {
   return (
     <div className='partnerships'>
         <p className='font-size-3rem-bolder color-white'>Partnerships</p>
-        <p className='font-size-1_5rem color-dull'>Partner with us! and let us grow together.</p>
+        <p className='font-size-1_5rem color-dull'>Partner with us! Let's grow together.</p>
+        <br/>
+        <DiscordButton  value="Get in Touch"/>
         <img src={eth}/>
         <img src={bitcoin}/>
     </div>
