92e8711b235efadee616033df9f6b03ee63392d2
Aviroxi <gurugkgurukiran@gmail.com>
2022-07-19 16:17:56 +0530

    added

--- src/components/LandingPage/subcomponents/ZScore.js
+++ src/components/LandingPage/subcomponents/ZScore.js
@@ -7,7 +7,7 @@ export default function ZScore() {
         <p className='font-size-1_5rem color-white'>Introducing</p>
         <p className='font-size-3rem-bolder color-white'>Web3.0 Identity with <span className='gradientext8'>zScore</span></p>
         <p className='font-size-1_5rem color-dull'>By increasing your zScore, You can receive high LTV.</p>
-        <img src={gauge} width={300}/>
+        <img src={gauge}/>
     </div>
   )
 }
